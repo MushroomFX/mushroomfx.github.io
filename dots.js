7ea8c952be4223e7e36082f3da3b8ec9dc82ca32
MushroomFX <dergolem26@gmail.com>
2022-12-25 03:39:25 +0100

    + First real commit

---
+++ dots.js
@@ -0,0 +1,44 @@
+var canvas = document.getElementById('background');
+var dots = []
+var amt = .5
+function genDots(){
+  dots = []
+  var w = window.innerWidth
+  var h = window.innerHeight
+
+  canvas.setAttribute('width',w+"px")
+  canvas.setAttribute('height',h+"px")
+
+  var ctx = canvas.getContext('2d');
+
+  for(i=0;i<(h*w)/5000;i++){
+    dots.push({
+      x:Math.random()*w*2-w*0.5,
+      y:Math.random()*h*2-h*0.5,
+      w:Math.random()*amt,
+    })
+  }
+  render(0,0)
+}
+genDots()
+
+onmousemove = function(e){
+  render(e.clientX,e.clientY)
+}
+function render(eX,eY){
+  var ctx = canvas.getContext('2d');
+  var offset = {
+    x:eX-(window.innerWidth/2),
+    y:eY-(window.innerHeight/2)
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for(i=0;i<dots.length;i++){
+    ctx.beginPath()
+    ctx.fillStyle = `rgba(255 255 255 / ${100 - (1 + amt / (dots[i].w))**3}%)`
+    ctx.arc(dots[i].x + (dots[i].w * offset.x),dots[i].y + (dots[i].w * 2 * offset.y),1+dots[i].w,0,2*Math.PI)
+    ctx.fill()
+  }
+}
+window.onresize = genDots
+document.body.addEventListener("resize", genDots());
---
+++ index.js
@@ -0,0 +1,44 @@
+for(i=0;i<document.querySelectorAll('.animLetters').length;i++){
+    var current = document.querySelectorAll('.animLetters')[i]
+    var arr = current.innerHTML.split('')
+    var temp = []
+    for(j=0;j<arr.length;j++){
+        temp.push(`<span class="animLetter">${arr[j]}</span>`)
+    }
+    current.innerHTML = temp.join('')
+}
+for(i=0;i<document.querySelectorAll('.animLetter').length;i++){
+    var time = Math.floor(Math.random()*1000)+1000
+    var offset = Math.floor(Math.random()*100)+100
+    var elem = document.querySelectorAll('.animLetter')[i]
+    elem.onmouseleave  = (event) => {remHoverGlow(event.target)};
+    elem.onmouseover = (event) => {hoverGlow(event.target)};
+    elem.style.position = 'relative'
+    elem.style.top = `${offset}px`;
+    elem.style.opacity = '0'
+    elem.style.filter = 'blur(10px)'
+    elem.style.transition = `top ${time}ms, opacity ${time}ms, filter ${time}ms, text-shadow 250ms`;
+    turnVisible(elem)
+}
+function turnVisible(elem){
+    setTimeout(function(){
+        elem.style.top = '0px'
+        elem.style.opacity = '1'
+        elem.style.filter = 'blur(0px)'
+    },0)
+}
+
+function remHoverGlow(elem){
+    elem.style.textShadow = 'none';
+    if(elem.previousElementSibling !== null){elem.previousElementSibling.style.textShadow = 'none';}
+    if(elem.nextElementSibling !== null){elem.nextElementSibling.style.textShadow = 'none';}
+}
+
+function hoverGlow(elem){
+    var prev = elem.previousElementSibling;
+    var next = elem.nextElementSibling;
+
+    elem.style.textShadow = `0px 0px 10px ${window.getComputedStyle(elem).color}`;
+    if(elem.previousElementSibling !== null){prev.style.textShadow = `0px 0px 5px ${window.getComputedStyle(prev).color}`;}
+    if(elem.nextElementSibling !== null){next.style.textShadow = `0px 0px 5px ${window.getComputedStyle(next).color}`;}
+}
