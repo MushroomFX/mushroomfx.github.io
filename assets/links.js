edb18769a9bdcbaa31cccbbcf1a61270faebb0fb
MushroomFX <dergolem26@gmail.com>
2022-12-26 05:14:42 +0100

    + Formated links.js

--- assets/links.js
+++ assets/links.js
@@ -1,10 +1,9 @@
 function openDC() {
-    window.open("http://discord.com/users/1056399841372164107/");
-  }
-  function openGit() {
-    window.open("http://github.com/MushroomFX/");
-  }
-  function openPen() {
-    window.open("https://codepen.io/MushroomFX/");
-  }
-  
+  window.open("http://discord.com/users/1056399841372164107/");
+}
+function openGit() {
+  window.open("http://github.com/MushroomFX/");
+}
+function openPen() {
+  window.open("https://codepen.io/MushroomFX/");
+}
